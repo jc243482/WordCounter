41f03fd82d2f9d4e1afded170bae1bc5449b131a
jc243482 <james.baker@my.jcu.edu.au>
2015-03-09 16:00:12 +1000

    Added method to count the number of words in the text file

--- src/App.java
+++ src/App.java
@@ -3,7 +3,19 @@
  */
 public class App {
     public static void main(String[] args) {
+        System.out.println("Manual unit test of WordCounter");
+        System.out.println("simple.txt has 5 words");
         String text = TextReader.load("simple.txt");
+        int tally = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
+                tally ++;
+            }else {
+
+            }
+        }
+        int numberOfWords = tally + 1;
+        System.out.println("Actual number of words: " + numberOfWords);
         System.out.println(text);
     }
 }
