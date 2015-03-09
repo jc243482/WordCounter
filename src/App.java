669cb5a3fe527aec4e571f943a3b33467b8d8db2
jc243482 <james.baker@my.jcu.edu.au>
2015-03-09 16:48:01 +1000

    Counts lines, words and marks

--- src/App.java
+++ src/App.java
@@ -3,26 +3,51 @@
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Manual unit test of WordCounter \n simple.txt has 5 words");
+        System.out.println("Manual unit test of WordCounter \n simple.txt");
         String text = TextReader.load("simple.txt");
         int numberOfWords = countWords(text);
-        System.out.println("Actual number of words: " + numberOfWords);
-        System.out.println(text);
+        int numberOfLines = countLines(text);
+        int numberOfMarks = countMarks(text);
+        System.out.println("Actual number of words: " + numberOfWords + "\n" + "Actual number of lines: " + numberOfLines + "\n" + "Actual number of punctuation marks: " + numberOfMarks);
 
-        System.out.println("Manual unit test of WordCounter \n multiline.txt has 9 words");
+        System.out.println("Manual unit test of WordCounter \n multiline.txt ");
         text = TextReader.load("multiline.txt");
         numberOfWords = countWords(text);
-        System.out.println("Actual number of words: " + numberOfWords);
-        System.out.println(text);
+        numberOfLines = countLines(text);
+        numberOfMarks = countMarks(text);
+        System.out.println("Actual number of words: " + numberOfWords + "\n" + "Actual number of lines: " + numberOfLines + "\n" + "Actual number of punctuation marks: " + numberOfMarks);
     }
 
     private static int countWords(String text) {
-        int tally = 0;
+        int tallyWords = 0;
         for (int i = 0; i < text.length(); i++){
             if (text.charAt(i) == ' ' || text.charAt(i) == '\n') {
-                tally++;
+                tallyWords++;
+            }else {
+
             }
-        }
-        return tally + 1;
+        }return tallyWords + 1;
+
+
+    }
+    private static int countLines(String text) {
+        int tallyLines = 0;
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == '\n'){
+                tallyLines++;
+            }else {
+
+            }
+        }return tallyLines + 1;
+    }
+    private static int countMarks(String text){
+        int tallyMarks = 0;
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == '!' || text.charAt(i) == ':' || text.charAt(i) == ';' || text.charAt(i) == '?' || text.charAt(i) == '.' || text.charAt(i) == ','){
+                tallyMarks++;
+            }else{
+
+            }
+        }return tallyMarks;
     }
 }
