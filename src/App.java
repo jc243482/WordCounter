46cef570d954c3b9e3dd0d91d52e8ada79a8b206
jc243482 <james.baker@my.jcu.edu.au>
2015-03-09 16:08:55 +1000

    Added method to count the number of words in the multiline text file

--- src/App.java
+++ src/App.java
@@ -17,5 +17,20 @@ public class App {
         int numberOfWords = tally + 1;
         System.out.println("Actual number of words: " + numberOfWords);
         System.out.println(text);
+
+        System.out.println("Manual unit test of WordCounter");
+        System.out.println("multiline.txt has 9 words");
+        String text2 = TextReader.load("multiline.txt");
+        int tally2 = 0;
+        for (int i = 0; i < text2.length(); i++) {
+            if (text2.charAt(i) == ' ') {
+                tally2 ++;
+            }else if (text2.charAt(i) == '\n') {
+                tally2 ++;
+            }
+        }
+        int numberOfWords2 = tally2 + 1;
+        System.out.println("Actual number of words: " + numberOfWords2);
+        System.out.println(text2);
     }
 }
