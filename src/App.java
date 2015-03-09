3927c5e5a52f482bf02743f6d2e0500758e730a2
jc243482 <james.baker@my.jcu.edu.au>
2015-03-09 16:19:56 +1000

    changed the main method to call the count words method

--- src/App.java
+++ src/App.java
@@ -3,34 +3,26 @@
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Manual unit test of WordCounter");
-        System.out.println("simple.txt has 5 words");
+        System.out.println("Manual unit test of WordCounter \n simple.txt has 5 words");
         String text = TextReader.load("simple.txt");
-        int tally = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == ' ') {
-                tally ++;
-            }else {
-
-            }
-        }
-        int numberOfWords = tally + 1;
+        int numberOfWords = countWords(text);
         System.out.println("Actual number of words: " + numberOfWords);
         System.out.println(text);
 
-        System.out.println("Manual unit test of WordCounter");
-        System.out.println("multiline.txt has 9 words");
-        String text2 = TextReader.load("multiline.txt");
-        int tally2 = 0;
-        for (int i = 0; i < text2.length(); i++) {
-            if (text2.charAt(i) == ' ') {
-                tally2 ++;
-            }else if (text2.charAt(i) == '\n') {
-                tally2 ++;
+        System.out.println("Manual unit test of WordCounter \n multiline.txt has 9 words");
+        text = TextReader.load("multiline.txt");
+        numberOfWords = countWords(text);
+        System.out.println("Actual number of words: " + numberOfWords);
+        System.out.println(text);
+    }
+
+    private static int countWords(String text) {
+        int tally = 0;
+        for (int i = 0; i < text.length(); i++){
+            if (text.charAt(i) == ' ' || text.charAt(i) == '\n') {
+                tally++;
             }
         }
-        int numberOfWords2 = tally2 + 1;
-        System.out.println("Actual number of words: " + numberOfWords2);
-        System.out.println(text2);
+        return tally + 1;
     }
 }
